5a59ffcce9ee374f9905aac9cf3ff1e254a68b4b
Rahul.Choubey <Rukmani@7593>
2023-05-10 09:51:02 +0100

    primary button class name change

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -536,12 +536,12 @@ export function decorateButtons(element) {
       const twoup = a.parentElement.parentElement;
       if (!a.querySelector('img')) {
         if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.className = 'button primary'; // default
+          a.className = 'button primary - 1'; // default
           up.classList.add('button-container');
         }
         if (up.childNodes.length === 1 && up.tagName === 'STRONG'
           && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button primary';
+          a.className = 'button primary - 1';
           twoup.classList.add('button-container');
         }
         if (up.childNodes.length === 1 && up.tagName === 'EM'
